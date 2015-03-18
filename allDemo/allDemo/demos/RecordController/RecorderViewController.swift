c8589f9bda49e3206abef5b8516d24bc68e7025c
nahaowan <ylx123456@gmail.com>
2015-03-18 15:19:42 +0800

    add recorder

---
+++ allDemo/allDemo/demos/RecordController/RecorderViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RecordController.swift
+//  allDemo
+//
+//  Created by nahaowan on 15/3/17.
+//  Copyright (c) 2015年 nahaowan. All rights reserved.
+//
+
+import UIKit
+
+class RecordController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
